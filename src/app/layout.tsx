c3cd299af508731fcbfb0bd34f9ea508498e97b1
0xyshv <writetoyshv@gmail.com>
2024-01-30 10:51:33 +0530

    feat: add user type routing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "SBTify-User",
+  title: "SovID",
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,29 @@ import {
   Typography,
   Tooltip,
 } from "@material-tailwind/react";
+import Link from "next/link";
 
 import React from "react";
 
 export default function Homepage() {
   return (
     <>
-      <Card className="mt-6 w-96" placeholder="">
-        Are you a user or Admin?
+      <Card className="mt-6 w-96 flex flex-col justify-around" placeholder="">
+        <p className="flex justify-center">Are you a user or Admin?</p>
+        <CardBody placeholder="">
+          <div className="flex justify-around">
+            <Link href="/user">
+              <div className="p-4 border border-black rounded-lg shadow-lg inline-block cursor-pointer">
+                User
+              </div>
+            </Link>
+            <Link href="/admin">
+              <div className="p-4 border border-black rounded-lg shadow-lg inline-block cursor-pointer">
+                Admin
+              </div>
+            </Link>
+          </div>
+        </CardBody>
       </Card>
     </>
   );
