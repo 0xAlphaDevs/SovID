7877fdba453cc17e3f85713fca077c6bff2fc277
0xyshv <writetoyshv@gmail.com>
2024-01-30 13:33:28 +0530

    feat: add modal for user dashboard table

---
+++ src/components/user/dashboard/view-modal.tsx
@@ -0,0 +1,80 @@
+import React, { use } from "react";
+import {
+  Button,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
+} from "@material-tailwind/react";
+import { useContractRead } from "wagmi";
+import { sbts } from "@/constants/sbt";
+
+interface ViewModalProps {
+  sbtName: string;
+  sbtAddress: string;
+  tokenId: string;
+}
+
+export function ViewModal({ sbtName, sbtAddress, tokenId }: ViewModalProps) {
+  const [open, setOpen] = React.useState(false);
+
+  const { data, isRefetching, refetch } = useContractRead({
+    address: sbts[sbtAddress].sbtAddress,
+    abi: sbts[sbtAddress].abi,
+    functionName: "verifyCredential",
+    args: [tokenId],
+    onSuccess: (data: any) => {
+      console.log(data);
+    },
+  });
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <>
+      <Button
+        onClick={handleOpen}
+        color="green"
+        variant="gradient"
+        placeholder=""
+      >
+        View
+      </Button>
+      <Dialog open={open} handler={handleOpen} placeholder="">
+        <DialogHeader
+          className="flex justify-center items-center"
+          placeholder=""
+        >
+          {sbtName}
+        </DialogHeader>
+        <DialogBody placeholder="">
+          <div className="bg-green-200 w-96 p-8 rounded-lg shadow-xl max-w-sm mx-auto border deep-purple-700">
+            <ul>
+              {Object.keys(data).map((field: string) => (
+                <li key={field}>
+                  <strong>
+                    {field} : {String(data?.[field])}
+                  </strong>{" "}
+                  {/* Add static values here if available */}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </DialogBody>
+        <DialogFooter placeholder="">
+          <Button
+            variant="gradient"
+            color="green"
+            onClick={handleOpen}
+            fullWidth
+            placeholder=""
+          >
+            <span>Close</span>
+          </Button>
+        </DialogFooter>
+      </Dialog>
+    </>
+  );
+}
