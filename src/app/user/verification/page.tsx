70fa0d90529a0e5c1c28b0d866aac42ca1646524
0xyshv <writetoyshv@gmail.com>
2024-01-30 18:52:21 +0530

    chore: data update for /verification

--- src/app/user/verification/page.tsx
+++ src/app/user/verification/page.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Connected } from "@/components/connected";
+import UserNavbar from "@/components/user/navbar";
+import VerificationTable from "@/components/user/verification/verification-table";
 
 const Verification = () => {
-  return <div>Verification</div>;
+  return (
+    <>
+      <Connected>
+        <UserNavbar />
+        <div className="p-8 flex flex-col gap-4">
+          <VerificationTable />
+        </div>
+      </Connected>
+    </>
+  );
 };
 
 export default Verification;
