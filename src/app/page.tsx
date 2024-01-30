09efb2b6f423814793e25ee4eaa730c248b1b34e
0xyshv <writetoyshv@gmail.com>
2024-01-30 10:38:22 +0530

    chore: add routes for user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 "use client";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Typography,
+  Tooltip,
+} from "@material-tailwind/react";
 
-import React, { useEffect } from "react";
+import React from "react";
 
 export default function Homepage() {
   return (
     <>
-      <div> Hello World</div>
+      <Card className="mt-6 w-96" placeholder="">
+        Are you a user or Admin?
+      </Card>
     </>
   );
 }
