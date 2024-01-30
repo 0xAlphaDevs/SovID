4fd129f4f4187a780808a2dfea9c10ce530b859e
0xyshv <writetoyshv@gmail.com>
2024-01-30 11:12:26 +0530

    fix: config

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,7 +4,7 @@ import { ConnectKitProvider } from "connectkit";
 import * as React from "react";
 import { WagmiConfig } from "wagmi";
 
-import { config } from "../wagmi";
+import { config } from "../lib/config";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [mounted, setMounted] = React.useState(false);
