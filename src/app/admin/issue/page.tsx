ea41e081c50f167ce63de989eb7798ef7f81dab1
0xyshv <writetoyshv@gmail.com>
2024-02-02 18:02:11 +0530

    chore: update issue page

--- src/app/admin/issue/page.tsx
+++ src/app/admin/issue/page.tsx
@@ -1,13 +1,24 @@
+"use client";
+
 import React from "react";
+import { useSearchParams } from "next/navigation";
 import { Connected } from "@/components/connected";
 import AdminNavbar from "@/components/admin/navbar";
+import StepperIssueSbt from "@/components/admin/issue/stepper";
 
 const Issue = () => {
+  const searchParams = useSearchParams();
+
+  const tokenName = searchParams.get("tokenName");
+  const tokenAddress = searchParams.get("tokenAddress");
   return (
     <>
       <Connected>
         <AdminNavbar />
-        <div className="p-8 flex flex-col gap-4">Issue</div>
+        <div className="p-8 flex flex-col gap-4">
+          {" "}
+          <StepperIssueSbt tokenName={tokenName} tokenAddress={tokenAddress} />
+        </div>
       </Connected>
     </>
   );
