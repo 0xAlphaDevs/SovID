415a07e7c35b9776abb3bb4ca1cb07b0e809da87
Harsh Tyagi <writetoharshtyagi@gmail.com>
2024-02-03 11:51:21 +0530

    fix: fix div at botom

--- src/components/admin/issue/fill-sbt-details.tsx
+++ src/components/admin/issue/fill-sbt-details.tsx
@@ -62,91 +62,93 @@ export default function FillSbtDetails({
   };
 
   return (
-    <div className="w-96">
-      <form onSubmit={issueCredential}>
-        {isLoading ? (
-          <div className="mt-8">
-            <DefaultSpinner />
-          </div>
-        ) : (
-          <>
-            <div className="space-y-12">
-              <div className="border-b border-gray-900/10 pb-12">
-                <h2 className="text-base font-semibold leading-7 text-gray-900 mt-4">
-                  Credential Information for {sbts[tokenName].sbtName}
-                </h2>
-                <p className="mt-1 text-sm leading-6 text-gray-600">
-                  Please make sure you fill all the details correctly.
-                </p>
+    <>
+      <div className="w-96">
+        <form onSubmit={issueCredential}>
+          {isLoading ? (
+            <div className="mt-8">
+              <DefaultSpinner />
+            </div>
+          ) : (
+            <>
+              <div className="space-y-12">
+                <div className="border-b border-gray-900/10 pb-12">
+                  <h2 className="text-base font-semibold leading-7 text-gray-900 mt-4">
+                    Credential Information for {sbts[tokenName].sbtName}
+                  </h2>
+                  <p className="mt-1 text-sm leading-6 text-gray-600">
+                    Please make sure you fill all the details correctly.
+                  </p>
 
-                <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                  {sbts[tokenName].sbtFields.map((field: any) => (
+                  <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    {sbts[tokenName].sbtFields.map((field: any) => (
+                      <div className="sm:col-span-6">
+                        <label className="block text-sm font-medium leading-6 text-gray-900">
+                          {field.title}
+                        </label>
+                        <div className="mt-2">
+                          <input
+                            type={field.type}
+                            required={true}
+                            value={formData[field.title]}
+                            onChange={(e) => {
+                              setFormData({
+                                ...formData,
+                                [field.title]: e.target.value,
+                              });
+                            }}
+                            autoComplete="given-name"
+                            className="block w-full rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                          />
+                        </div>
+                      </div>
+                    ))}
+
+                    {/* Issue to wallet address */}
                     <div className="sm:col-span-6">
                       <label className="block text-sm font-medium leading-6 text-gray-900">
-                        {field.title}
+                        Issue to wallet address
                       </label>
-                      <div className="mt-2">
-                        <input
-                          type={field.type}
-                          required={true}
-                          value={formData[field.title]}
-                          onChange={(e) => {
-                            setFormData({
-                              ...formData,
-                              [field.title]: e.target.value,
-                            });
-                          }}
-                          autoComplete="given-name"
-                          className="block w-full rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                      </div>
+                      <input
+                        type="string"
+                        required={true}
+                        value={formData["Wallet Address"]}
+                        onChange={(e) => {
+                          setFormData({
+                            ...formData,
+                            ["Wallet Address"]: e.target.value,
+                          });
+                          setIsValid(validateEthAddress(e.target.value));
+                        }}
+                        autoComplete="given-name"
+                        className="block w-full rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                      {isValid ? (
+                        <p className="text-green-600">Valid Ethereum Address</p>
+                      ) : (
+                        <p className="text-red-600">
+                          Enter valid Ethereum Address
+                        </p>
+                      )}
                     </div>
-                  ))}
-
-                  {/* Issue to wallet address */}
-                  <div className="sm:col-span-6">
-                    <label className="block text-sm font-medium leading-6 text-gray-900">
-                      Issue to wallet address
-                    </label>
-                    <input
-                      type="string"
-                      required={true}
-                      value={formData["Wallet Address"]}
-                      onChange={(e) => {
-                        setFormData({
-                          ...formData,
-                          ["Wallet Address"]: e.target.value,
-                        });
-                        setIsValid(validateEthAddress(e.target.value));
-                      }}
-                      autoComplete="given-name"
-                      className="block w-full rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                    {isValid ? (
-                      <p className="text-green-600">Valid Ethereum Address</p>
-                    ) : (
-                      <p className="text-red-600">
-                        Enter valid Ethereum Address
-                      </p>
-                    )}
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="flex justify-center mt-4">
-              <Button
-                placeholder=""
-                type="submit"
-                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                disabled={!isValid}
-              >
-                Issue SBT
-              </Button>
-            </div>
-          </>
-        )}
-      </form>
-      <div className="absolute top-10 right-5">
+              <div className="flex justify-center mt-4">
+                <Button
+                  placeholder=""
+                  type="submit"
+                  className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  disabled={!isValid}
+                >
+                  Issue SBT
+                </Button>
+              </div>
+            </>
+          )}
+        </form>
+      </div>
+      <div className="fixed bottom-10 right-5">
         {true && (
           <Alert
             open={open}
@@ -169,6 +171,6 @@ export default function FillSbtDetails({
           </Alert>
         )}
       </div>
-    </div>
+    </>
   );
 }
