eb54381307b48d088ce80900326127750be10ae5
0xyshv <writetoyshv@gmail.com>
2024-02-03 22:35:22 +0530

    chore: sync

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,44 @@
 "use client";
 
-import React, { useEffect } from "react";
-import { useAccount } from "wagmi";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { ConnectKitButton } from "connectkit";
 import {
+  Button,
   Card,
   CardBody,
   CardFooter,
   Typography,
 } from "@material-tailwind/react";
 import HomeNavbar from "@/components/navbar";
+import { useAccount, useContractRead } from "wagmi";
+import { authorizedUserTokenContractConfig } from "@/lib/contracts";
 
 const Admin = () => {
   const { address, isDisconnected } = useAccount();
   const router = useRouter();
+  const [isUnregistered, setIsUngistered] = useState(false);
 
-  React.useEffect(() => {
-    if (address) {
-      router.push("/");
-    }
-  }, [address]);
+  const { error, isLoading, isSuccess } = useContractRead({
+    ...authorizedUserTokenContractConfig,
+    functionName: "getVerifiedUserMetadata",
+    args: [address],
+    onSuccess: (data: any) => {
+      if (data?.userName === "") {
+        // router.push("/");
+        setIsUngistered(true);
+      } else if (data?.category === "individual") {
+        router.push("/user/dashboard");
+      } else router.push("/admin/dashboard");
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
 
   return (
     <>
-      {isDisconnected && (
+      {address && (
         <>
           <HomeNavbar />
           <div className="text-center">
@@ -43,26 +57,44 @@ const Admin = () => {
                   Connect your wallet to get started.
                   <br />
                   <br />
-                  {/* In case you are a User,{" "}
-                  <a href="/user" className="cursor-pointer underline">
-                    Click here
-                  </a>{" "}
-                  to go to user dashboard. */}
                 </Typography>
-                {/* <Typography placeholder="">
-                  <br />
-                  In case you are an Admin,{" "}
-                  <a href="/user" className="cursor-pointer underline">
-                    Click here
-                  </a>{" "}
-                  to go to admin dashboard.
-                </Typography> */}
               </CardBody>
               <CardFooter className="pt-0 flex justify-center" placeholder="">
                 <ConnectKitButton />
               </CardFooter>
             </Card>
           </div>
+          {isUnregistered && (
+            <Card placeholder="" className="mt-6 mx-20 py-8">
+              <Typography
+                placeholder=""
+                variant="h5"
+                color="blue-gray"
+                className="mb-2"
+              >
+                Register as
+              </Typography>
+              <div>
+                <Button
+                  placeholder=""
+                  onClick={() => {
+                    router.push("/user");
+                  }}
+                >
+                  User
+                </Button>
+                <Button
+                  placeholder=""
+                  className="ml-8"
+                  onClick={() => {
+                    router.push("/admin");
+                  }}
+                >
+                  Admin
+                </Button>
+              </div>
+            </Card>
+          )}
         </>
       )}
     </>
